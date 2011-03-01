901093e70e6dac02a62c9a58bfce8624e948b31b
Adam Jacques <adam@adamjacques.com>
2011-03-01 19:02:28 +0000

    Created the Word_SendVia addin for Office 2010 to add a quick button to send a document using NexusIM
    git-svn-id: https://svn.adrensoftware.com/svn/InstantMessage@62 1bd42360-39e3-e847-8c3f-f219214e882b

---
+++ Word_SendVia/Ribbon.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Office = Microsoft.Office.Core;
+using Word = Microsoft.Office.Interop.Word;
+using System.Windows.Forms;
+
+namespace Word_SendVia
+{
+	[ComVisible(true)]
+	public class CustomRibbon : Office.IRibbonExtensibility
+	{
+		private Office.IRibbonUI ribbon;
+
+		#region IRibbonExtensibility Members
+
+		public string GetCustomUI(string ribbonID)
+		{
+			return GetResourceText("Word_SendVia.Ribbon.xml");
+		}
+
+		#endregion
+
+		#region Ribbon Callbacks
+		//Create callback methods here. For more information about adding callback methods, select the Ribbon XML item in Solution Explorer and then press F1
+
+		public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+		{
+			this.ribbon = ribbonUI;
+		}
+
+		public void SendAsAttach(Office.IRibbonControl control)
+		{
+			Mutex testMutex = new Mutex(false, "Local\\NexusIM");
+			bool isRunning = !testMutex.WaitOne(0, false);
+			testMutex.Close();
+			Word.Document document = Globals.ThisAddIn.Application.Application.ActiveDocument;
+			string fileName = document.FullName;
+
+			if (isRunning)
+			{
+				NamedPipeClientStream cStream = new NamedPipeClientStream("nexusim");
+				BinaryWriter writer = new BinaryWriter(cStream);
+				writer.Write(IPCMessageTypes.SendFile);
+			}
+		}
+
+		#endregion
+
+		#region Helpers
+
+		private static string GetResourceText(string resourceName)
+		{
+			Assembly asm = Assembly.GetExecutingAssembly();
+			string[] resourceNames = asm.GetManifestResourceNames();
+			for (int i = 0; i < resourceNames.Length; ++i)
+			{
+				if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+				{
+					using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+					{
+						if (resourceReader != null)
+						{
+							return resourceReader.ReadToEnd();
+						}
+					}
+				}
+			}
+			return null;
+		}
+
+		#endregion
+	}
+
+	internal static class IPCMessageTypes
+	{
+		public static const byte SendFile = 0x01;
+	}
+}
---
+++ Word_SendVia/ThisAddIn.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using Ribbon = Microsoft.Office.Tools.Ribbon;
+
+namespace Word_SendVia
+{
+	public partial class ThisAddIn
+	{
+		private void ThisAddIn_Startup(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void ThisAddIn_Shutdown(object sender, EventArgs e)
+		{
+		}
+
+		protected override Office.IRibbonExtensibility CreateRibbonExtensibilityObject()
+		{
+			return new CustomRibbon();
+		}
+
+		#region VSTO generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InternalStartup()
+		{
+			this.Startup += new System.EventHandler(ThisAddIn_Startup);
+			this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+		}
+		
+		#endregion
+	}
+}
